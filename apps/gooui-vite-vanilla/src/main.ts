da17a42d4007ad5d69db354f0fcb48280b307e66
Konrad Reczko <reczkok@gmail.com>
2026-02-26 13:45:03 +0100

    pbr jelly

--- apps/gooui-vite-vanilla/src/main.ts
+++ apps/gooui-vite-vanilla/src/main.ts
@@ -1,13 +1,24 @@
 import './style.css';
 import { CanvasJellySlider } from 'gooui';
 import tgpu from 'typegpu';
-import { wood } from './wood-slider.ts';
+// import { createWood } from "./wood-slider.ts";
+// import { createCobble } from "./cobble-slider.ts";
+import { createGold } from './gold-slider.ts';
+// import { createGrass } from "./grass-slider.ts";
 
 const appElement = document.querySelector('#app') as HTMLDivElement;
 const root = await tgpu.init();
 
+// const wood = await createWood(root);
+// const cobble = await createCobble(root);
+const gold = await createGold(root);
+// const grass = await createGrass(root);
+
 const slider = new CanvasJellySlider({
   root,
-  ...wood,
+  // ...wood,
+  // ...cobble,
+  ...gold,
+  // ...grass,
 });
 appElement.appendChild(slider.canvas);
